fcbb46034ac25f79e0ac2ddde135325f41d02ea6
Padunk <abraham.agel@gmail.com>
2020-01-23 17:27:02 +0700

    add chat_app and snake_game app

--- hello_world/hello_world.rs
+++ hello_world/hello_world.rs
@@ -1,3 +1,19 @@
+struct Rectangle {
+   width: u32,
+   height: u32,
+}
+
 fn main() {
-   println!("Hello, World !");
+   let rect = Rectangle {
+      width: 32,
+      height: 10,
+   };
+
+   let area = rectangle(&rect);
+
+   println!("area {}", area);
+}
+
+fn rectangle(r: &Rectangle) -> u32 {
+   r.width * r.height
 }
